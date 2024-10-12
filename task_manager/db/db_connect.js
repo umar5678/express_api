d591646b6531f6f5973524dce57e0ab694ddcd96
Umar Farooq <umarfarooq567800@gmail.com>
2024-10-12 13:07:43 +0500

    db

--- task_manager/db/db_connect.js
+++ task_manager/db/db_connect.js
@@ -5,11 +5,6 @@ import mongoose from "mongoose";
 const db_name = "tasks";
 
 const connectionStrig = process.env.TASK_MANG_MONGODB_URI;
-<<<<<<< HEAD
-
-=======
-console.log(connectionStrig);
->>>>>>> 059b94a033f1bcd8f69ec125bd8a5110d57c8545
 
 const connectDB = async () => {
   try {
